fede207a3b32a4c5fd1a766296276e18b540d0c4
Anton Kovalev <antoha91011@mail.ru>
2022-12-05 01:27:28 +0700

    домашнее задание

---
+++ Задание1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+string numberFirstStr = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string numberSecondStr = Console.ReadLine();
+
+int numberFirst = Convert.ToInt32(numberFirstStr);
+int numberSecond = Convert.ToInt32(numberSecondStr);
+
+if (numberFirst>numberSecond)
+     Console.WriteLine(numberFirstStr);
+      else
+       Console.WriteLine(numberSecondStr);
---
+++ Задание1/Задание2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое числа");
+string number1Str=Console.ReadLine();
+Console.WriteLine("Введите второе числа");
+string number2Str=Console.ReadLine();
+Console.WriteLine("Введите третье числа");
+string number3Str=Console.ReadLine();
+
+int number1 = Convert.ToInt32(number1Str);
+int number2 = Convert.ToInt32(number2Str);
+int number3 = Convert.ToInt32(number3Str);
+
+if (number1 > number2)
+if (number1 > number3)
+{
+    Console.WriteLine(number1);
+}
+if (number2 > number1)
+if (number2 > number3)
+{
+     Console.WriteLine(number2);
+}
+if (number3 > number1)
+if (number3 > number2)
+{
+     Console.WriteLine(number3);
+}
